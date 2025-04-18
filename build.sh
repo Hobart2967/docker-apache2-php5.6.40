ffd428fa72432cc8cc288c86d76719419072d585
Hobart2967 <hobart2967@outlook.com>
2025-04-18 02:52:04 +0200

    ci(core): :construction_worker: Add CI

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build . -t docker-apache2-php5.6.40:$1
+docker build . -t docker-apache2-php5.6.40:latest
---
+++ release.sh
@@ -0,0 +1,2 @@
+docker push docker-apache2-php5.6.40:$1
+docker push docker-apache2-php5.6.40:latest
