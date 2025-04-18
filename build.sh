184176cbd08bfce68e22c7ec15e4883da8bff720
Hobart2967 <hobart2967@outlook.com>
2025-04-18 11:06:13 +0200

    fix: Fix image name.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build . -t docker-apache2-php5.6.40:$1
-docker build . -t docker-apache2-php5.6.40:latest
+docker build . -t hobart2967/docker-apache2-php5.6.40:$1
+docker build . -t hobart2967/docker-apache2-php5.6.40:latest
